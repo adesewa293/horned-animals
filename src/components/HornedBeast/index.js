2202d093fa8cd1bda5648131e5fb37b3d4ca3896
Adesewa Aishat <adesewaaishat@gmail.com>
2023-07-18 17:21:54 +0100

    new changes

--- src/components/HornedBeast/index.js
+++ src/components/HornedBeast/index.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./hornedbeast.css";
 
 function HornedBeast(props) {
   const { title, imageUrl, description } = props;
+  const [fav, setFav] = useState(0);
+
+function handleFav() {
+   setFav(fav+1);
+}
+
+
   return (
     <div className="beast-container">
       <h2>{title}</h2>
       <img src={imageUrl} alt={title} title={title} />
       <p>{description}</p>
+      <span className="likeButton" onClick={handleFav}>❤️</span> {fav}
     </div>
   );
 }
